2c643dbcac6cb750c906775c655c70cdf938f843
michiru6828 <since_l999@yahoo.co.jp>
2023-09-14 15:45:00 -0700

    removed unneccesary code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ReactNode } from 'react'
 
